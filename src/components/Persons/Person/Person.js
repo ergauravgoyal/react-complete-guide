e4ce8bfe0fb5daef8cf34a12235d9e4044dfc161
Gaurav Goyal <gauravg@XEBIAINDIA.LOCAL>
2018-07-17 17:33:50 +0530

    Converted Stateless to Stateful Component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Radium from "radium";
 import myPersonClass from "./Person.css";
-const person = props => {
-  // const style = {
-  //   "@media(min-width:500px)": {
-  //     width: "450px"
-  //   }
-  // };
-  // const rnd = Math.random();
-  // if (rnd > 0.9) {
-  //   throw new Error("Something went wrong");
-  // }
-  return (
-    <div className={myPersonClass.Person}>
-      <p onClick={props.click}>
-        I'm a {props.name} and {props.age} year Old
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
 
-export default Radium(person);
+class Person extends Component {
+  render() {
+    return (
+      <div className={myPersonClass.Person}>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name} and {this.props.age} year Old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
+
+export default Radium(Person);
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
-import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
-const persons = props =>
-  props.persons.map((person, index) => {
-    return (
-      <ErrorBoundary key={person.id}>
+
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
         <Person
           key={person.id}
-          click={() => props.clicked(index)}
+          click={() => this.props.clicked(index)}
           name={person.name}
           age={person.age}
-          changed={event => props.changed(event, person.id)}
+          changed={event => this.props.changed(event, person.id)}
         />
-      </ErrorBoundary>
-    );
-  });
+      );
+    });
+  }
+}
 
-export default persons;
+export default Persons;
